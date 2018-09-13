6b17e3cdc3b24db927154172e8a746ade52a0d7c
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-09-14 01:14:50 +0530

    add Landing skeletal

---
+++ src/App/Component/Landing/SelectPorts/Destination/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Destination extends Component { // eslint-disable-line
+    render() {
+        return (
+            <div>select destination port</div>
+        );
+    }
+}
+
+export default Destination;
