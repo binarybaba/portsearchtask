9e32544490c26c94a7bb42d069a3cbb8e2cc0eec
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-09-14 01:15:40 +0530

    add select ports skeletal

---
+++ src/App/Component/Landing/SelectPorts/Origin/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Origin extends Component { // eslint-disable-line
+    render() {
+        return (
+            <div>select origin port</div>
+        );
+    }
+}
+
+export default Origin;
