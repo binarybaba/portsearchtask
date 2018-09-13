599a82a3cb7ed4823caaee1eeb11638644c33475
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-09-14 03:55:54 +0530

    add promised debounce

--- src/App/Util.js
+++ src/App/Util.js
@@ -1,7 +1,24 @@
-const WAIT_MILLISECONDS = 200;
+const WAIT_MILLISECONDS = 300;
 let timeoutId;
 
-const debounce = (fn) => {
+
+/*
+* Returns a debounced resolved promise to be .then() chained later on
+* */
+const debounce = fn => new Promise((resolve) => {
+    const later = () => {
+        resolve(fn);
+        timeoutId = null;
+    };
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(later, WAIT_MILLISECONDS);
+});
+
+/*
+* Returns a debounced function to be immediately used
+* */
+
+const debouncePlain = (fn) => {
     const later = () => {
         fn();
         timeoutId = null;
@@ -11,5 +28,6 @@ const debounce = (fn) => {
 };
 
 export {
-    debounce, // eslint-disable-line
+    debouncePlain, // eslint-disable-line
+    debounce,
 };
