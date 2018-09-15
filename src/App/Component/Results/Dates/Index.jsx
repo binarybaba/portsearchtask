9ba0020c2e8e7d5859a8beef2b689889efcc4229
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-09-15 19:02:11 +0530

    add date picker component

---
+++ src/App/Component/Results/Dates/Index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import { MIN_FROM_DATE, MAX_TO_DATE } from '../../../CONSTANT';
+
+class Dates extends Component { // eslint-disable-line
+
+    state={};
+
+    componentDidMount() {
+        const { from, to } = this.props;
+        this.setState(() => ({
+            from: new Date(from),
+            to: new Date(to),
+        }));
+        // this.setState(() => this.getInitialState());
+    }
+
+    handleDayClick(day) {
+        const range = DateUtils.addDayToRange(day, this.state);
+        const { setDateRange } = this.props;
+        this.setState(range);
+        if (setDateRange) {
+            setDateRange({ ...range });
+        }
+    }
+
+    render() {
+        const { from, to } = this.state;
+        const modifiers = { start: from, end: to };
+        return (
+            <DayPicker
+                className="Selectable"
+                month={from ? new Date(from.getFullYear(), from.getMonth()) : null}
+                numberOfMonths={2}
+                selectedDays={[from, { from, to }]}
+                modifiers={modifiers}
+                onDayClick={day => this.handleDayClick(day)}
+                disabledDays={{ after: MAX_TO_DATE, before: MIN_FROM_DATE }}
+            />
+        );
+    }
+}
+
+export default Dates;
