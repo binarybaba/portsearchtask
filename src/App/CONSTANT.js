1202bdae90c8aebbaa8ec3939b7a4a6d5978ea4a
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-09-15 21:24:23 +0530

    Add global constants

---
+++ src/App/CONSTANT.js
@@ -0,0 +1,11 @@
+export const BASE_URL = '/api';
+
+export const BACKGROUND_COLOR = '#f8f8f9';
+export const BORDER_COLOR = '#dedede';
+export const HIGHLIGHTED_BLACK = '#323756';
+
+export const DEFAULT_FROM_DATE = '2017-08-30';
+export const DEFAULT_TO_DATE = '2018-06-30';
+
+export const MIN_FROM_DATE = new Date('2017-08-30');
+export const MAX_TO_DATE = new Date('2018-06-30');
